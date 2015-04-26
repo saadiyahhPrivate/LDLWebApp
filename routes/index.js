6d46f09f20e05ad221315c3e5c82379088e9cd82
Dana Mukusheva <d.mukusheva@gmail.com>
2015-04-25 22:53:57 -0400

    get results

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,30 @@ var mongoose = require('mongoose');
 
 var Model = require("../models/Model")
 /* GET home page. */
-router.get('/', function(req, res){
-	res.render('index', { title: 'MMDX'});
-});
-/**
+
+
+
 router.get('/results', function(req, res) {
   var results = req.query;
-  var new_data = new Model({location:{lat:results.lat, lng: results.lon}, diagnosis:results.diagnosis});
-  new_data.save(function(err){
-  	if (err){
-  		res.json({
-                statusCode: 500, 
-                message: "Error creating a new data instance"
-            });
+  console.log(results);
+  if (!results.lat){
+  	   	res.render('index', { title: 'MMDX', lat: "Unknown", lng: "Unknown", diagnosis: "Unknown"});
   	}
-  	else{
-  	   res.render('index', { title: 'MMDX', lat: results.lat, lng: results.lng, diagnosis: results.diagnosis });
-  	}
-  });
 
-  res.render('index', { title: 'MMDX', lat: results.lat, lng: results.lng, diagnosis: results.diagnosis });
+  else{
+	  var new_data = new Model({location:{lat:results.lat, lng: results.lon}, diagnosis:results.diagnosis});
+	  new_data.save(function(err){
+	  	if (err){
+	  		res.json({
+	                statusCode: 500, 
+	                message: "Error creating a new data instance"
+	            });
+	  	}
+	  	else{
+	  	   	res.render('index', { title: 'MMDX', lat: results.lat, lng: results.lng, diagnosis: results.diagnosis });	   	
+	  	}
+	  });
+  }
 });
-*/
+
 module.exports = router;
