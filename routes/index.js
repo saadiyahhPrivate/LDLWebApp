786ae385462070ef90483f0ea156b2d76cdbb607
Dana Mukusheva <d.mukusheva@gmail.com>
2015-04-25 22:44:43 -0400

    routes and views

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,26 @@ var mongoose = require('mongoose');
 
 var Model = require("../models/Model")
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res){
+	res.render('index', { title: 'MMDX'});
 });
+/**
+router.get('/results', function(req, res) {
+  var results = req.query;
+  var new_data = new Model({location:{lat:results.lat, lng: results.lon}, diagnosis:results.diagnosis});
+  new_data.save(function(err){
+  	if (err){
+  		res.json({
+                statusCode: 500, 
+                message: "Error creating a new data instance"
+            });
+  	}
+  	else{
+  	   res.render('index', { title: 'MMDX', lat: results.lat, lng: results.lng, diagnosis: results.diagnosis });
+  	}
+  });
 
+  res.render('index', { title: 'MMDX', lat: results.lat, lng: results.lng, diagnosis: results.diagnosis });
+});
+*/
 module.exports = router;
