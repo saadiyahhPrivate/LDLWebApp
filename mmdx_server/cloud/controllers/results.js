875d4635f25e739f8ebc7a1192a52a5977ee0b55
saadiyahhPrivate <saadiyah@mit.edu>
2015-05-03 23:45:54 -0400

    trying to add d3.js map + html file for debugging

--- mmdx_server/cloud/controllers/results.js
+++ mmdx_server/cloud/controllers/results.js
@@ -1,29 +1,26 @@
 var Result = Parse.Object.extend('Result');
 
+exports.create = function(req, res) {
+	var results = req.query; //get data from query
+	var new_result = new Result();
 
+  	new_result.set("lat", results.lat);
+  	new_result.set("lng", results.lng);
+  	new_result.set("diagnosis", results.diagnosis);
 
-exports.receiveData = function(req, res) {
-  var results = req.body; //get data from query
-  alert(results);
-  var new_result = new Result();
-
-    new_result.set("lat", results.lat.toString());
-    new_result.set("lng", results.lng.toString());
-    new_result.set("diagnosis", results.diagnosis);
-
-    new_result.save(null, {
-      success: function(new_result) {
-      // Execute any logic that should take place after the object is saved.
-        alert('New object created with objectId: ' + new_result.id);
-        res.redirect('/');
-      },
-      error: function(new_result, error) {
-      // Execute any logic that should take place if the save fails.
-      // error is a Parse.Error with an error code and message.
-        alert('Failed to create new object, with error code: ' + error.message);
-        res.send(500, 'Failed creating Result object');
-      }
-  });
+  	new_result.save(null, {
+  		success: function(new_result) {
+    	// Execute any logic that should take place after the object is saved.
+    		alert('New object created with objectId: ' + new_result.id);
+    		res.redirect('/');
+  		},
+  		error: function(new_result, error) {
+	    // Execute any logic that should take place if the save fails.
+	    // error is a Parse.Error with an error code and message.
+	    	alert('Failed to create new object, with error code: ' + error.message);
+	    	res.send(500, 'Failed creating Result object');
+	    }
+	});
 }
 
 exports.showAll = function(req, res) {
